42f195c6d863697ff0529f299c4e23a360f3fbc8
Alan Christensen <alan@christensen.org.nz>
2011-11-21 22:26:58 +1300

    added integrationtests project. want to test persistence layer directly

---
+++ Source/IntegrationTests/Persistence/SupplierPersistence.cs
@@ -0,0 +1,20 @@
+using DDDIntro.Domain;
+using NUnit.Framework;
+using Persistence;
+
+namespace IntegrationTests.Persistence
+{
+    [TestFixture]
+    public class SupplierPersistence
+    {
+        [Test]
+        public void Action_Scenario_Behaviour()
+        {
+            // Arrange
+            new NHibernateUnitOfWorkFactory()
+            // Act
+
+            // Assert
+        }
+    }
+}
