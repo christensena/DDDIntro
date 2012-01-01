b735a020ed2212b4a6d769f241ab2d9ed8d30f0e
Alan Christensen <alan@christensen.org.nz>
2012-01-01 15:38:13 +1300

    unique constraint on purchase order number

--- Source/Persistence/MappingOverrides/PurchaseOrderMappingOverride.cs
+++ Source/Persistence/MappingOverrides/PurchaseOrderMappingOverride.cs
@@ -1,4 +1,3 @@
-using System;
 using DDDIntro.Domain;
 using FluentNHibernate.Automapping;
 using FluentNHibernate.Automapping.Alterations;
@@ -9,6 +8,8 @@ namespace DDDIntro.Persistence.MappingOverrides
     {
         public void Override(AutoMapping<PurchaseOrder> mapping)
         {
+            mapping.Map(x => x.OrderNumber).Unique();
+
             mapping.HasMany(x => x.OrderLines)
                 .Access.CamelCaseField() // without this, the orderLines backing field is not proxy populated correctly by NH, producing errors when manipulating the list of a rehydrated order
                 .AsList(x => x.Column("LineNumber")) // this means ordering is maintained but I don't need LineNumber cluttering+complicating my model
