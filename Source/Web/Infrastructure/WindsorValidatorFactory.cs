238d68963c6c653006892bb38c47ab5f1f342659
Alan Christensen <alan@christensen.org.nz>
2012-03-23 20:47:48 +1300

    tiny enhancement to validator factory

--- Source/Web/Infrastructure/WindsorValidatorFactory.cs
+++ Source/Web/Infrastructure/WindsorValidatorFactory.cs
@@ -16,16 +16,10 @@ namespace DDDIntro.Web.Infrastructure
 
         public override IValidator CreateInstance(Type validatorType)
         {
-            try
-            {
-                return container.Resolve(validatorType) as IValidator;
-            }
-            // yuck. may have to try another IoC container if can't conditionally resolve
-            // without exceptions
-            catch (ComponentNotFoundException)
-            {
+            if (!container.Kernel.HasComponent(validatorType))
                 return null;
-            }
+
+            return container.Resolve(validatorType) as IValidator;
         }
     }
 }
