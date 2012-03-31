1ba3751b49a581cca39de19ecf5e55a5824f5fd3
Alan Christensen <alan@christensen.org.nz>
2012-03-31 19:19:17 +1300

    added twitter bootstrap, assets/static content

--- Source/WebAPI/Infrastructure/NancyBootstrapper.cs
+++ Source/WebAPI/Infrastructure/NancyBootstrapper.cs
@@ -2,6 +2,7 @@ using System;
 using Castle.MicroKernel.Lifestyle;
 using Castle.Windsor.Installer;
 using Nancy.Bootstrappers.Windsor;
+using Nancy.Conventions;
 
 namespace DDDIntro.WebAPI.Infrastructure
 {
@@ -19,6 +20,14 @@ namespace DDDIntro.WebAPI.Infrastructure
             };
 
             pipelines.AfterRequest += context => lifestyleScope.Dispose();
+        }
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("Assets"));
         }
 
         protected override void ConfigureApplicationContainer(Castle.Windsor.IWindsorContainer existingContainer)
